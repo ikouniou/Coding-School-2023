35c8b49dd697e6bf635e94db4a3fb146a4906437
Ioanna K <joankou.kou@gmail.com>
2023-01-14 19:54:48 +0200

    Added exercise 2 with sum or product

--- Session-05/Session-05/Program.cs
+++ Session-05/Session-05/Program.cs
@@ -4,6 +4,35 @@ ReverseString reverseString = new ReverseString();
 Console.WriteLine(reverseString.Reverse());
 Console.WriteLine();
 
+Console.WriteLine("Give an integer:");
+var integer = Console.ReadLine();
+int number;
+bool isNumerical = int.TryParse(integer, out number);
+while(!isNumerical)
+{
+    Console.WriteLine("Not an integer, please try again");
+    integer = Console.ReadLine();
+    isNumerical = int.TryParse(integer, out number);
+}
+
+Console.WriteLine("For sum write 'sum' for product write 'product'");
+string operation = Console.ReadLine();
+while(operation != "sum" && operation != "product")
+{
+    Console.WriteLine("Please give an acceptable value");
+    operation = Console.ReadLine();
+}
+
+SumOrProduct sumOrProduct = new SumOrProduct();
+if(operation == "sum")
+{
+    Console.WriteLine(sumOrProduct.Sum(number));
+}
+else 
+{
+    Console.WriteLine(sumOrProduct.Product(number));
+}
+
 
 
 
---
+++ Session-05/Session-05/SumOrProduct.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class SumOrProduct
+{
+    /*   2. Write a C# program that asks the user for an integer (n) and gives them
+         the possibility to choose between computing the sum and computing
+         the product of 1,…,n.2.
+	*/
+
+
+    public int Sum(int number)
+	{
+		int sum = 0;
+		for(int i  = 1; i <= number; i++) 
+		{
+			sum += i;
+		}
+
+		return sum;
+	}
+
+	public int Product(int number) 
+	{
+		int product = 1;
+		for(int i = 1; i <= number; i++)
+		{
+			product *= i;
+		}
+
+		return product;
+
+	}
+}
