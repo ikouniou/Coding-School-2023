abfd9c2c341b0a46ea7ccf7de948070106bcdfa9
Ioanna K <joankou.kou@gmail.com>
2023-02-02 15:43:14 +0200

    Added one more example with animals list

---
+++ Session-19/Session-19/Animal.cs
@@ -0,0 +1,9 @@
+﻿namespace Session_19 {
+    public class Animal {
+
+        public string? Type { get; set; }
+        public string? Name { get; set; }
+        public string? OwnerName { get; set; }
+
+    }
+}
---
+++ Session-19/Session-19/Controllers/AnimalController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Session_19.Controllers {
+    [ApiController]
+    [Route("[controller]")]
+    public class AnimalController : ControllerBase{
+
+        [HttpGet(Name = "GetAnimal")]
+        public List<Animal> Get() {
+
+            List<Animal> animals = new List<Animal>();
+
+            Animal animal1 = new Animal() {
+                Name = "Max",
+                Type = "Dog",
+                OwnerName = "Ioanna"
+            };
+
+            Animal animal2 = new Animal() {
+                Name = "Betty",
+                Type = "Hamster",
+                OwnerName = "Ioanna"
+            };
+
+            animals.Add(animal1);
+            animals.Add(animal2);
+
+            return animals;
+        }
+
+    }
+}
