9fafaa1e26700d99204b05ad704e5f45453fcdfb
Ioanna K <joankou.kou@gmail.com>
2023-02-02 15:26:33 +0200

    Try first Service example and create Person controller

---
+++ Session-19/Session-19/Controllers/PersonController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Session_19.Controllers {
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase {
+
+        [HttpGet(Name = "GetPerson")]
+        public Person Get() {
+
+
+            Person person = new Person() {
+                Name = "Ioanna",
+                Surname = "Kouniou"
+            };
+
+            return person;
+        }
+    }
+}
