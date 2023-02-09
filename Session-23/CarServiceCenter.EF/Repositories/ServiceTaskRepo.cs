db9d70161985b44ef39810c7fd3b6021666a677b
Ioanna K <joankou.kou@gmail.com>
2023-02-09 20:55:03 +0200

    Added service task repo

---
+++ Session-23/CarServiceCenter.EF/Repositories/ServiceTaskRepo.cs
@@ -0,0 +1,54 @@
+﻿using CarServiceCenter.EF.Context;
+using CarServiceCenter.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarServiceCenter.EF.Repositories {
+    public class ServiceTaskRepo : IEntityRepo<ServiceTask> {
+        public void Add(ServiceTask entity) {
+
+            using var context = new CarServiceCenterDbContext();
+            context.Add(entity);
+            context.SaveChanges();
+        }
+
+        public void Delete(int id) {
+
+            using var context = new CarServiceCenterDbContext();
+            var dbServiceTask = context.ServiceTasks.Where(serviceTask => serviceTask.Id == id).SingleOrDefault();
+            if (dbServiceTask is null) {
+                return;
+            }
+            context.Remove(dbServiceTask);
+            context.SaveChanges();
+        }
+
+        public IList<ServiceTask> GetAll() {
+
+            using var context = new CarServiceCenterDbContext();
+            return context.ServiceTasks.ToList();
+        }
+
+        public ServiceTask? GetById(int id) {
+
+            using var context = new CarServiceCenterDbContext();
+            return context.ServiceTasks.Where(serviceTask => serviceTask.Id == id).SingleOrDefault();
+        }
+
+        public void Update(int id, ServiceTask entity) {
+
+            using var context = new CarServiceCenterDbContext();
+            var dbServiceTask = context.ServiceTasks.Where(serviceTask => serviceTask.Id == id).SingleOrDefault();
+            if (dbServiceTask is null) {
+                return;
+            }
+            dbServiceTask.Code = entity.Code;
+            dbServiceTask.Description = entity.Description;
+            dbServiceTask.Hours = entity.Hours;
+            context.SaveChanges();
+        }
+    }
+}
