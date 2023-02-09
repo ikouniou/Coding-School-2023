5bea8c15d2c0bf844fefdf829554adf34d826264
Ioanna K <joankou.kou@gmail.com>
2023-02-09 21:04:38 +0200

    Added transaction repo

---
+++ Session-23/CarServiceCenter.EF/Repositories/TransactionRepo.cs
@@ -0,0 +1,53 @@
+﻿using CarServiceCenter.EF.Context;
+using CarServiceCenter.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarServiceCenter.EF.Repositories {
+    public class TransactionRepo : IEntityRepo<Transaction> {
+        public void Add(Transaction entity) {
+
+            using var context = new CarServiceCenterDbContext();
+            context.Add(entity);
+            context.SaveChanges();
+        }
+
+        public void Delete(int id) {
+
+            using var context = new CarServiceCenterDbContext();
+            var dbTransaction = context.Transactions.Where(transaction => transaction.Id == id).SingleOrDefault();
+            if (dbTransaction is null) {
+                return;
+            }
+            context.Remove(dbTransaction);
+            context.SaveChanges();
+        }
+
+        public IList<Transaction> GetAll() {
+
+            using var context = new CarServiceCenterDbContext();
+            return context.Transactions.ToList();
+        }
+
+        public Transaction? GetById(int id) {
+
+            using var context = new CarServiceCenterDbContext();
+            return context.Transactions.Where(transaction => transaction.Id == id).SingleOrDefault();
+        }
+
+        public void Update(int id, Transaction entity) {
+
+            using var context = new CarServiceCenterDbContext();
+            var dbTransaction = context.Transactions.Where(transaction => transaction.Id == id).SingleOrDefault();
+            if (dbTransaction is null) {
+                return;
+            }
+            dbTransaction.Date = entity.Date;
+            dbTransaction.TotalPrice = entity.TotalPrice;
+            context.SaveChanges();
+        }
+    }
+}
