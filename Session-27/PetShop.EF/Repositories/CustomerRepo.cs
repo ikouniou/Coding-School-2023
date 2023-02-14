660b72ea11db5729d11705c08ba79c241a1bf1ed
xtipou <xtipou@gmail.com>
2023-02-14 13:10:22 +0200

    Customer & Pet Repos

---
+++ Session-27/PetShop.EF/Repositories/CustomerRepo.cs
@@ -0,0 +1,67 @@
+﻿using PetShop.EF.Context;
+using PetShop.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShop.EF.Repositories
+{
+    public class CustomerRepo
+    {
+        public void Add(Customer entity)
+        {
+            using var context = new PetShopDbContext();
+            if (entity.Id != 0)
+            {
+                throw new ArgumentException("Given entity should not have Id set", nameof(entity));
+                context.Add(entity);
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using var context = new PetShopDbContext();
+            var dbCustomer = context.Customers.SingleOrDefault(customer => customer.Id == id);
+            if (dbCustomer is null)
+            {
+                throw new KeyNotFoundException($"Given id '{id}' was not found in database");
+            }
+            context.Remove(dbCustomer);
+            context.SaveChanges();
+
+        }
+
+        public IList<Customer> GetAll()
+        {
+            using var context = new PetShopDbContext();
+            return context.Customers.ToList();
+        }
+
+
+        public Customer? GetById(int id)
+        {
+            using var context = new PetShopDbContext();
+            return context.Customers.SingleOrDefault(customer => customer.Id == id);
+
+        }
+        public void Update(int id, Customer entity)
+        {
+            using var context = new PetShopDbContext();
+            var dbCustomer = context.Customers.SingleOrDefault(customer => customer.Id == id);
+            if (dbCustomer is null)
+            {
+                throw new KeyNotFoundException($"Given id '{id}' was not found in database");
+            }
+            dbCustomer.Phone = entity.Phone;
+            dbCustomer.Name = entity.Name;
+            dbCustomer.Surname = entity.Surname;
+            dbCustomer.Tin = entity.Tin;
+            context.SaveChanges();
+        }
+
+    }
+  
+}
---
+++ Session-27/PetShop.EF/Repositories/PetRepo.cs
@@ -0,0 +1,62 @@
+﻿using PetShop.EF.Context;
+using PetShop.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShop.EF.Repositories
+{
+    public class PetRepo
+    {
+        public void Add (Pet entity)
+        {
+            using var context = new PetShopDbContext();
+            if (entity.Id != 0)
+                throw new ArgumentException("Given entity should not have Id set", nameof(entity));
+            context.Add(entity);
+            context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            using var context = new PetShopDbContext();
+            var dbPet = context.Pets.SingleOrDefault(pet => pet.Id == id);
+            if (dbPet is null)
+            {
+                throw new KeyNotFoundException($"Given id '{id}' was not found in database");
+            }
+            context.Remove(dbPet);
+            context.SaveChanges();
+
+        }
+        public IList<Pet> GetAll()
+        {
+            using var context = new PetShopDbContext();
+            return context.Pets.ToList();
+        }
+
+        public Pet? GetById(int id)
+        {
+            using var context = new PetShopDbContext();
+            return context.Pets.SingleOrDefault(pet => pet.Id == id);
+
+        }
+        public void Update(int id, Pet entity)
+        {
+            using var context = new PetShopDbContext();
+            var dbPet = context.Pets.SingleOrDefault(pet => pet.Id == id);
+            if (dbPet is null)
+            {
+                throw new KeyNotFoundException($"Given id '{id}' was not found in database");
+            }
+            dbPet.Cost=entity.Cost;
+            dbPet.PetStatus = entity.PetStatus;
+            dbPet.AnimalType = entity.AnimalType;
+            dbPet.Breed = entity.Breed;
+            dbPet.Cost = entity.Cost;
+            context.SaveChanges();
+        }
+    }
+}
