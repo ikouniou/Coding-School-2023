bedfdae21406cbda4cb07b5d3596b03b9c9ee43b
Ioanna K <joankou.kou@gmail.com>
2023-02-14 15:11:43 +0200

    Added petFood DTOs and api controller

---
+++ Session-27/PetShop.Web.Blazor/Server/Controllers/PetFoodController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PetShop.EF.Repositories;
+using PetShop.Model;
+using PetShop.Web.Blazor.Shared.Pet;
+
+namespace PetShop.Web.Blazor.Server.Controllers {
+    [Route("[controller]")]
+    [ApiController]
+    public class PetFoodController : ControllerBase {
+        private readonly IEntityRepo<PetFood> _petFoodRepo;
+
+        public PetFoodController(IEntityRepo<PetFood> petFoodRepo)
+        {
+            _petFoodRepo = petFoodRepo;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<PetFoodListDto>> Get()
+        {
+            var result = _petFoodRepo.GetAll();
+            return result.Select(petFood => new PetFoodListDto
+            {
+                Id = petFood.Id,
+                AnimalType = petFood.AnimalType,
+                Price = petFood.Price,
+                Cost = petFood.Cost
+            });
+        }
+
+        [HttpGet("{id}")]
+        public async Task<PetFoodEditDto> GetById(int id)
+        {
+            var result = _petFoodRepo.GetById(id);
+            return new PetFoodEditDto
+            {
+                Id = id,
+                AnimalType = result.AnimalType,
+                Price = result.Price,
+                Cost = result.Cost
+            };
+        }
+
+        [HttpPost]
+        public async Task Post(PetFoodEditDto petFood)
+        {
+            var newPetFood = new PetFood(petFood.AnimalType, petFood.Price, petFood.Cost);
+            _petFoodRepo.Add(newPetFood);
+        }
+
+        [HttpPut]
+        public async Task Put(PetFoodEditDto petFood)
+        {
+            var itemToUpdate = _petFoodRepo.GetById(petFood.Id);
+            itemToUpdate.AnimalType = petFood.AnimalType;
+            itemToUpdate.Price = petFood.Price;
+            itemToUpdate.Cost = petFood.Cost;
+            _petFoodRepo.Update(petFood.Id, itemToUpdate);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task Delete(int id)
+        {
+            _petFoodRepo.Delete(id);
+        }
+    }
+}
---
+++ Session-27/PetShop.Web.Blazor/Shared/PetFood/PetFoodListDto.cs
@@ -0,0 +1,15 @@
+﻿using PetShop.Model.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShop.Web.Blazor.Shared.Pet {
+    public class PetFoodListDto {
+        public int Id { get; set; }
+        public AnimalType AnimalType { get; set; }
+        public decimal Price { get; set; }
+        public decimal Cost { get; set; }
+    }
+}
