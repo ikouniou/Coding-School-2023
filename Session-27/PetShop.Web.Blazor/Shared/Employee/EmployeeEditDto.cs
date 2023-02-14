79e284756409a6643903931b17837d140833e8f5
Christos Kontorias <45541430+christosKontorias@users.noreply.github.com>
2023-02-14 15:05:53 +0200

    +EmployeeEditDto

---
+++ Session-27/PetShop.Web.Blazor/Shared/Employee/EmployeeEditDto.cs
@@ -0,0 +1,16 @@
+﻿using PetShop.Model.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShop.Web.Blazor.Shared.Employee {
+    public class EmployeeEditDto {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public EmployeeType EmployeeType { get; set; }
+        public int SalaryPerMonth { get; set; }
+    }
+}
