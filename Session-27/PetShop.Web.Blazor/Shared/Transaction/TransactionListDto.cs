a1383ef9bcfd8d037f9effe7342150a8bd0dba62
AnestisKountourgiannis <Anekoujohn@gmail.com>
2023-02-14 18:44:49 +0200

    Small change

--- Session-27/PetShop.Web.Blazor/Shared/Transaction/TransactionListDto.cs
+++ Session-27/PetShop.Web.Blazor/Shared/Transaction/TransactionListDto.cs
@@ -9,7 +9,7 @@ namespace PetShop.Web.Blazor.Shared.Transaction {
         public int Id { get; set; }
         public DateTime Date { get; set; }
         public decimal PetPrice { get; set; }
-        public int PetFoodQty { get; set }
+        public int PetFoodQty { get; set; }
         public decimal PetFoodPrice { get; set; }
         public decimal TotalPrice { get; set; }
     }
