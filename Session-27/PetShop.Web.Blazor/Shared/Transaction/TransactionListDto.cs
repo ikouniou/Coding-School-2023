f92b7bcffc9983ce670a4fcd6f9817bc27fd1df6
AnestisKountourgiannis <Anekoujohn@gmail.com>
2023-02-14 18:36:17 +0200

    TransactionListDto check

--- Session-27/PetShop.Web.Blazor/Shared/Transaction/TransactionListDto.cs
+++ Session-27/PetShop.Web.Blazor/Shared/Transaction/TransactionListDto.cs
@@ -6,5 +6,11 @@ using System.Threading.Tasks;
 
 namespace PetShop.Web.Blazor.Shared.Transaction {
     public class TransactionListDto {
+        public int Id { get; set; }
+        public int Date { get; set; }
+        public decimal PetPrice { get; set; }
+        public int PetFoodQty { get; set }
+        public decimal PetFoodPrice { get; set; }
+        public decimal TotalPrice { get; set; }
     }
 }
