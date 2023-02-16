6ae06f4ccd745db176c0d16f3cc6fc3a6206bc9c
Christos Kontorias <45541430+christosKontorias@users.noreply.github.com>
2023-02-16 17:57:15 +0200

    PetReport, Edit Index

--- Session-27/PetShop.Web.Blazor/Server/Controllers/PetReportController.cs
+++ Session-27/PetShop.Web.Blazor/Server/Controllers/PetReportController.cs
@@ -1,7 +1,40 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PetShop.EF.Repositories;
+using PetShop.Model;
+using PetShop.Model.Enums;
+using PetShop.Web.Blazor.Shared.PetReport;
 
 namespace PetShop.Web.Blazor.Server.Controllers {
-	public class PetReportController : ControllerBase{
 
-	}
+    [Route("[controller]")]
+    [ApiController]
+    public class PetReportController : ControllerBase{
+        private readonly IEntityRepo<Transaction> _transactionRepo;
+        private readonly IEntityRepo<Customer> _customerRepo;
+        private readonly IEntityRepo<Pet> _petRepo;
+        private readonly IEntityRepo<PetFood> _petFoodRepo;
+
+        public PetReportController(IEntityRepo<Transaction> transactionRepo, IEntityRepo<Customer> customerRepo, IEntityRepo<Pet> petRepo, IEntityRepo<PetFood> petFoodRepo) {
+            _transactionRepo = transactionRepo;
+            _customerRepo = customerRepo;
+            _petRepo = petRepo;
+            _petFoodRepo = petFoodRepo;
+        }
+
+        [HttpGet]
+        public IEnumerable<PetReportListDto> Get(int year, int month, AnimalType animalType) {
+            var transactions = _transactionRepo.GetAll()
+                .Where(t => t.Date.Year == year && t.Date.Month == month);
+
+            var pets = _petRepo.GetAll()
+                .Where(p => p.AnimalType == animalType);
+
+            var petReportList = pets.Select(pet => new PetReportListDto {
+                AnimalType = pet.AnimalType,
+                TotalSold = transactions.Count(t => t.PetId == pet.Id)
+            }).ToList();
+
+            return petReportList;
+        }
+    }
 }
--- Session-27/PetShop.Web.Blazor/Shared/PetReport/PetReportListDto.cs
+++ Session-27/PetShop.Web.Blazor/Shared/PetReport/PetReportListDto.cs
@@ -1,11 +1,36 @@
-﻿using System;
+﻿using PetShop.Model.Enums;
+using PetShop.Web.Blazor.Shared.Transaction;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PetShop.Web.Blazor.Shared.PetReport {
-	public class PetReportListDto {
+    public class PetReportListDto {
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public AnimalType AnimalType { get; set; }
+        public int TotalSold { get; set; }
 
-	}
+        //public static List<PetReportListDto> FromTransactions(IEnumerable<TransactionListDto> transactions, int year, int month) {
+        //    var result = new List<PetReportListDto>();
+
+        //    foreach (AnimalType animalType in Enum.GetValues(typeof(AnimalType))) {
+        //        int totalSold = transactions
+        //            .Where(t => t.Date.Year == year && t.Date.Month == month)
+        //            .Sum(t => t.PetFoodQty);
+
+        //        result.Add(new PetReportListDto {
+        //            Year = year,
+        //            Month = month,
+        //            AnimalType = animalType,
+        //            TotalSold = totalSold
+        //        });
+        //    }
+
+        //    return result;
+        //}
+
+    }
 }
