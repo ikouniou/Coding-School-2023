bb0bcea46060684bf978ca7209c9de83a6a8abe3
Ioanna K <joankou.kou@gmail.com>
2023-02-16 20:40:56 +0200

    fix validation for all models

--- Session-27/PetShop.Web.Blazor/Shared/Employee/EmployeeEditDto.cs
+++ Session-27/PetShop.Web.Blazor/Shared/Employee/EmployeeEditDto.cs
@@ -10,7 +10,7 @@ namespace PetShop.Web.Blazor.Shared.Employee {
     public class EmployeeEditDto {
         public int Id { get; set; }
         [Required]
-        [MaxLength(50, ErrorMessage = "Name must have max length 50 digits.")]
+        [MaxLength(50, ErrorMessage = "Name must have max length 50 letters.")]
         public string Name { get; set; }
         [Required]
         [MaxLength(50, ErrorMessage = "Surname must have max length 50 letters.")]
