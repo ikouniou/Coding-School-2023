d3383d269bd2a00c681cb5be871b4a155eec1f6a
Ioanna K <joankou.kou@gmail.com>
2023-02-16 21:24:37 +0200

    small validation and fix in transaction create/edit form

--- Session-27/PetShop.Web.Blazor/Shared/PetFood/PetFoodEditDto.cs
+++ Session-27/PetShop.Web.Blazor/Shared/PetFood/PetFoodEditDto.cs
@@ -11,11 +11,11 @@ namespace PetShop.Web.Blazor.Shared.PetFood {
         public int Id { get; set; }
 		[Required]
         public AnimalType AnimalType { get; set; }
-		[RegularExpression(@"^(0|[1-9][0-9]?)(.\d{1,2})?$", ErrorMessage = "Invalid Price Number. Must have precision (4,2).")]
-		[Range(1, 99, ErrorMessage = "Invalid Price Number. Must have precision (4,2).")]
+		//[RegularExpression(@"^(0|[1-9][0-9]?)(.\d{1,2})?$", ErrorMessage = "Invalid Price Number. Must have precision (4,2).")]
+		//[Range(1, 99, ErrorMessage = "Invalid Price Number. Must have precision (4,2).")]
 		public decimal Price { get; set; }
-		[RegularExpression(@"^(0|[1-9][0-9]?)(.\d{1,2})?$", ErrorMessage = "Invalid Cost Number. Must have precision (4,2).")]
-		[Range(1,99, ErrorMessage = "Invalid Price Number. Must have precision (4,2).")]
+		//[RegularExpression(@"^(0|[1-9][0-9]?)(.\d{1,2})?$", ErrorMessage = "Invalid Cost Number. Must have precision (4,2).")]
+		//[Range(1,99, ErrorMessage = "Invalid Price Number. Must have precision (4,2).")]
 		public decimal Cost { get; set; }
     }
 }
--- Session-27/PetShop.Web.Blazor/Shared/Transaction/TransactionEditDto.cs
+++ Session-27/PetShop.Web.Blazor/Shared/Transaction/TransactionEditDto.cs
@@ -14,15 +14,12 @@ namespace PetShop.Web.Blazor.Shared.Transaction {
     public class TransactionEditDto 
     {
 		public int Id { get; set; }
-		[Required]
 		public DateTime Date { get; set; }
-		[Required]
 		public decimal PetPrice { get; set; }
 		[Required]
 		[Range(1, 99)]
 		public int PetFoodQty { get; set; }
 		public int SoldFoodQty { get; set; }
-		[Required]
 		public decimal PetFoodPrice { get; set; }
 		public decimal TotalPrice { get; set; }
 		public int CustomerId { get; set; }
