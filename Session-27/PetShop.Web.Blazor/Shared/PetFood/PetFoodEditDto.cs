083731483320f0e7436d0ab84acb9dcebc4ffec1
Ioanna K <joankou.kou@gmail.com>
2023-02-16 20:11:03 +0200

    Added validation for precision decimals

--- Session-27/PetShop.Web.Blazor/Shared/PetFood/PetFoodEditDto.cs
+++ Session-27/PetShop.Web.Blazor/Shared/PetFood/PetFoodEditDto.cs
@@ -1,6 +1,7 @@
 ﻿using PetShop.Model.Enums;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,13 @@ using System.Threading.Tasks;
 namespace PetShop.Web.Blazor.Shared.PetFood {
     public class PetFoodEditDto {
         public int Id { get; set; }
+		[Required]
         public AnimalType AnimalType { get; set; }
-        public decimal Price { get; set; }
-        public decimal Cost { get; set; }
+		[RegularExpression(@"^(0|[1-9][0-9]?)(.\d{1,2})?$", ErrorMessage = "Invalid Cost Number.")]
+		[Range(1, 99)]
+		public decimal Price { get; set; }
+		[RegularExpression(@"^(0|[1-9][0-9]?)(.\d{1,2})?$", ErrorMessage = "Invalid Cost Number.")]
+		[Range(1,99)]
+		public decimal Cost { get; set; }
     }
 }
