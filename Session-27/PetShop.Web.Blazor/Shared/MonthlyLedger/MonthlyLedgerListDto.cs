2922691e133f5ff91e08eb188ae3c66191445461
xtipou <xtipou@gmail.com>
2023-02-16 12:11:14 +0200

    bindings update

---
+++ Session-27/PetShop.Web.Blazor/Shared/MonthlyLedger/MonthlyLedgerListDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShop.Web.Blazor.Shared.MonthlyLedger
+{
+    public class MonthlyLedgerListDto
+    {
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public Decimal Income { get; set; }
+        public Decimal Expenses { get; set; }
+        public Decimal Total { get; set; }
+		public MonthlyLedgerListDto(DateTime date)
+		{
+			Year = date.Year;
+			Month = date.Month;
+		}
+
+		public void UpdateLedger(decimal income, decimal expenses)
+		{
+			Income = income;
+			Expenses = expenses;
+			Total = income - expenses;
+		}
+	}
+}
