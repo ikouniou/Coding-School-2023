2c594a073ca30d0a07515804ebaf75c15d8c0b74
Ioanna K <joankou.kou@gmail.com>
2023-02-18 22:42:31 +0200

    Added Fuel Station Db Context and configuration files

---
+++ Session-30/FuelStation.EF/Configurations/ItemConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using FuelStation.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuelStation.EF.Configurations {
+    public class ItemConfiguration : IEntityTypeConfiguration<Item> {
+        public void Configure(EntityTypeBuilder<Item> builder) {
+            throw new NotImplementedException();
+        }
+    }
+}
