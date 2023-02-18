d8e1a6a8ce21ef2afce500f7f84e87694df1ab27
Ioanna K <joankou.kou@gmail.com>
2023-02-19 00:10:46 +0200

    Transaction configuration completed

--- Session-30/FuelStation.EF/Configurations/TransactionConfiguration.cs
+++ Session-30/FuelStation.EF/Configurations/TransactionConfiguration.cs
@@ -10,7 +10,29 @@ using System.Threading.Tasks;
 namespace FuelStation.EF.Configurations {
     public class TransactionConfiguration : IEntityTypeConfiguration<Transaction> {
         public void Configure(EntityTypeBuilder<Transaction> builder) {
-            throw new NotImplementedException();
+
+            // Table Name
+            builder.ToTable("Transactions");
+
+            // Primary Key
+            builder.HasKey(transaction => transaction.Id);
+            builder.Property(transaction => transaction.Id).ValueGeneratedOnAdd();
+
+            // Properties
+            builder.Property(transaction => transaction.Date).IsRequired();
+            builder.Property(transaction => transaction.PaymentMethod).IsRequired();
+            builder.Property(transaction => transaction.TotalValue).HasPrecision(5,2).IsRequired();
+
+            //Relations
+            builder.HasOne(transaction => transaction.Customer)
+                .WithMany(customer => customer.Transactions)
+                .HasForeignKey(transaction => transaction.CustomerId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(transaction => transaction.Employee)
+                .WithMany(employee => employee.Transactions)
+                .HasForeignKey(transaction => transaction.EmployeeId)
+                .OnDelete(DeleteBehavior.Restrict);
         }
     }
 }
