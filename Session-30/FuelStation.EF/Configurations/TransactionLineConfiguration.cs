1603d7fc9de5e4ee18e52aaadfc9887682d6cf70
Ioanna K <joankou.kou@gmail.com>
2023-02-19 00:43:19 +0200

    Transaction Line configuration completed

--- Session-30/FuelStation.EF/Configurations/TransactionLineConfiguration.cs
+++ Session-30/FuelStation.EF/Configurations/TransactionLineConfiguration.cs
@@ -10,7 +10,32 @@ using System.Threading.Tasks;
 namespace FuelStation.EF.Configurations {
     public class TransactionLineConfiguration : IEntityTypeConfiguration<TransactionLine> {
         public void Configure(EntityTypeBuilder<TransactionLine> builder) {
-            throw new NotImplementedException();
+
+            // Table Name
+            builder.ToTable("TransactionLines");
+
+            // Primary Key
+            builder.HasKey(transactionLine => transactionLine.Id);
+            builder.Property(transactionLine => transactionLine.Id).ValueGeneratedOnAdd();
+
+            // Properties
+            builder.Property(transactionLine => transactionLine.Quantity).IsRequired();
+            builder.Property(transactionLine => transactionLine.ItemPrice).HasPrecision(5,2).IsRequired();
+            builder.Property(transactionLine => transactionLine.NetValue).HasPrecision(5,2).IsRequired();
+            builder.Property(transactionLine => transactionLine.DiscountPercent).IsRequired();
+            builder.Property(transactionLine => transactionLine.DiscountValue).HasPrecision(5,2).IsRequired();
+            builder.Property(transactionLine => transactionLine.TotalValue).HasPrecision(5,2).IsRequired();
+
+            //Relations
+            builder.HasOne(transactionLine => transactionLine.Transaction)
+                .WithMany(transaction => transaction.TransactionLines)
+                .HasForeignKey(transactionLine => transactionLine.TransactionId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(transactionLine => transactionLine.Item)
+                .WithMany(item => item.TransactionLines)
+                .HasForeignKey(transactionLine => transactionLine.ItemId)
+                .OnDelete(DeleteBehavior.Restrict);
         }
     }
 }
