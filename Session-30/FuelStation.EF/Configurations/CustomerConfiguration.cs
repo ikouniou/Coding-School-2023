d34edadfcc05e4ff5445bc80383faa0c31baa23e
Ioanna K <joankou.kou@gmail.com>
2023-02-18 23:39:33 +0200

    customer and employee configurations completed

--- Session-30/FuelStation.EF/Configurations/CustomerConfiguration.cs
+++ Session-30/FuelStation.EF/Configurations/CustomerConfiguration.cs
@@ -10,7 +10,18 @@ using System.Threading.Tasks;
 namespace FuelStation.EF.Configurations {
     public class CustomerConfiguration : IEntityTypeConfiguration<Customer> {
         public void Configure(EntityTypeBuilder<Customer> builder) {
-            
+
+            //Table Name
+            builder.ToTable("Customers");
+
+            //Primary Key
+            builder.HasKey(customer => customer.Id);
+            builder.Property(customer => customer.Id).ValueGeneratedOnAdd();
+
+            //Properties
+            builder.Property(customer => customer.Name).HasMaxLength(50).IsRequired();
+            builder.Property(customer => customer.Surname).HasMaxLength(100).IsRequired();
+            builder.Property(customer => customer.CardNumber).HasMaxLength(20).IsRequired();
 
         }
     }
--- Session-30/FuelStation.EF/Configurations/EmployeeConfiguration.cs
+++ Session-30/FuelStation.EF/Configurations/EmployeeConfiguration.cs
@@ -10,7 +10,21 @@ using System.Threading.Tasks;
 namespace FuelStation.EF.Configurations {
     public class EmployeeConfiguration : IEntityTypeConfiguration<Employee> {
         public void Configure(EntityTypeBuilder<Employee> builder) {
-            throw new NotImplementedException();
+
+            //Table Name
+            builder.ToTable("Employees");
+
+            //Primary Key
+            builder.HasKey(employee => employee.Id);
+            builder.Property(employee => employee.Id).ValueGeneratedOnAdd();
+
+            //Properties
+            builder.Property(employee => employee.Name).HasMaxLength(50).IsRequired();
+            builder.Property(employee => employee.Surname).HasMaxLength(100).IsRequired();
+            builder.Property(employee => employee.HireDateStart).IsRequired();
+            builder.Property(employee => employee.HireDateEnd).IsRequired();
+            builder.Property(employee => employee.SallaryPerMonth).IsRequired();
+            builder.Property(employee => employee.EmployeeType).IsRequired();
         }
     }
 }
