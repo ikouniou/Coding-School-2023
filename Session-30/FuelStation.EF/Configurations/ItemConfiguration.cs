c1c7763f1c23ffff1fe099c9dbd2edee47f7dc5d
Ioanna K <joankou.kou@gmail.com>
2023-02-18 23:57:54 +0200

    Item configuration completed

--- Session-30/FuelStation.EF/Configurations/ItemConfiguration.cs
+++ Session-30/FuelStation.EF/Configurations/ItemConfiguration.cs
@@ -10,7 +10,20 @@ using System.Threading.Tasks;
 namespace FuelStation.EF.Configurations {
     public class ItemConfiguration : IEntityTypeConfiguration<Item> {
         public void Configure(EntityTypeBuilder<Item> builder) {
-            throw new NotImplementedException();
+
+            //Table Name
+            builder.ToTable("Items");
+
+            //Primary Key
+            builder.HasKey(item => item.Id);
+            builder.Property(item => item.Id).ValueGeneratedOnAdd();
+
+            //Properties
+            builder.Property(item => item.Code).HasMaxLength(10).IsRequired();
+            builder.Property(item => item.Description).HasMaxLength(100).IsRequired();
+            builder.Property(item => item.ItemType).IsRequired();
+            builder.Property(item => item.Price).HasPrecision(5,2).IsRequired();
+            builder.Property(item => item.Cost).HasPrecision(5,2).IsRequired();
         }
     }
 }
