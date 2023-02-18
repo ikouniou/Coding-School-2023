62405564968fd600dc81bf6f4d006720ec0bf201
Ioanna K <joankou.kou@gmail.com>
2023-02-18 19:53:03 +0200

    Session-30 Initial Commit - Added model and classes

---
+++ Session-30/FuelStation.Model/Enums/EmployeeType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuelStation.Model.Enums {
+    public enum EmployeeType {
+
+        Manager = 1,
+        Staff = 2,
+        Cashier = 3
+    }
+}
