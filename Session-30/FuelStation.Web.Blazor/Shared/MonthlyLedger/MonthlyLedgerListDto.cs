56594247ccd636e18de06d3763ea75a5845d10e6
Ioanna K <joankou.kou@gmail.com>
2023-02-26 23:17:13 +0200

    Added monthly ledger method and page

---
+++ Session-30/FuelStation.Web.Blazor/Shared/MonthlyLedger/MonthlyLedgerListDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuelStation.Web.Blazor.Shared.MonthlyLedger {
+	public class MonthlyLedgerListDto {
+
+		public MonthlyLedgerListDto(DateTime date) {
+		    Year = date.Year;
+			Month = date.Month;
+		}
+		public int Year { get; set; }
+		public int Month { get; set; }
+		public decimal Income { get; set; }
+		public decimal Expenses { get; set; }
+		public decimal Total { get; set; }
+
+		public void UpdateValues(decimal income, decimal expenses) {
+			Income = income;
+			Expenses = expenses;
+			Total = income - expenses;
+		}
+	}
+}
